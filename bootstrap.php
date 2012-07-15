e2ffa72029c3d35ec6c0d2e9ae63d246c2c08d73
Derek Myers <arcticpro@gmail.com>
2012-07-14 20:29:25 -0700

    Fixing bootstrap classes

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,6 @@ Autoloader::add_core_namespace('Location');
 Autoloader::add_classes(array(
 	'Location\\Location'      => __DIR__.'/classes/location.php',
 	'Location\\Model_Country' => __DIR__.'/classes/model/country.php',
-	'Location\\Model_State'   => __DIR__.'/classes/model/State.php',
-	'Location\\Model_City'    => __DIR__.'/classes/model/City.php',
+	'Location\\Model_State'   => __DIR__.'/classes/model/state.php',
+	'Location\\Model_City'    => __DIR__.'/classes/model/city.php',
 ));
