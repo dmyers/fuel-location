a809c9c9385bfa6133a00ea50e788b5290803045
Derek Myers <arcticpro@gmail.com>
2013-06-25 15:33:55 -0500

    Changed model find method calls to use query method

--- classes/location.php
+++ classes/location.php
@@ -6,14 +6,14 @@ class Location
 {
 	public static function find_country($country_code)
 	{
-		return \Model_Country::find()
+		return \Model_Country::query()
 			->where('code', \Str::lower($country_code))
 			->get_one();
 	}
 
 	public static function find_state($country_code, $state_code)
 	{
-		return \Model_State::find()
+		return \Model_State::query()
 			->where('country_code', \Str::lower($country_code))
 			->where('code', \Str::lower($state_code))
 			->get_one();
@@ -21,7 +21,7 @@ class Location
 
 	public static function find_city($country_code, $state_code, $city_name)
 	{
-		return \Model_City::find()
+		return \Model_City::query()
 			->where('country_code', \Str::lower($country_code))
 			->where('state_code', \Str::lower($state_code))
 			->where('slug', \Inflector::friendly_title($city_name, '-', true))
@@ -36,7 +36,7 @@ class Location
 			return null;
 		}
 		
-		return \Model_City::find()
+		return \Model_City::query()
 			->where('country_code', \Str::lower($geo->country_code))
 			->where('state_code', \Str::lower($geo->region))
 			->where('name', $geo->city)
